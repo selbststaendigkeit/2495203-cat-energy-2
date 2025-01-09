35702d5e614b48835a432ee47f1ff8e68525f70e
selbststaendigkeit <141906217+selbststaendigkeit@users.noreply.github.com>
2025-01-09 18:14:09 +0300

    Задание 7.15 Декоративные элементы (#11)

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,18 @@
 /* в этот файл добавляет скрипты*/
+const menuElement = document.querySelector('.page-header__menu');
+const menuToggler = document.querySelector('.page-header__mobile-menu-toggler');
+
+const onTogglerClick = () => {
+  if (menuToggler.classList.contains('page-header__mobile-menu-toggler--closed')) {
+    menuToggler.classList.remove('page-header__mobile-menu-toggler--closed');
+    menuToggler.classList.add('page-header__mobile-menu-toggler--opened');
+    menuElement.classList.remove('page-header__menu--closed');
+    return;
+  }
+  menuToggler.classList.remove('page-header__mobile-menu-toggler--opened');
+  menuToggler.classList.add('page-header__mobile-menu-toggler--closed');
+  menuElement.classList.add('page-header__menu--closed');
+};
+
+menuElement.classList.remove('page-header__menu--nojs');
+menuToggler.addEventListener('click', onTogglerClick);
